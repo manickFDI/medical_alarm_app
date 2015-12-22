0c8ed82f5e66a5a6c299912c9f7ec6bdf5523dc5
manuma02 <manuma02@ucm.es>
2015-12-22 14:47:50 +0100

    login incorporado a nuestra app, quiza poner en otro thread

--- android_app/app/src/main/java/com/example/android/prueba/MainActivity.java
+++ android_app/app/src/main/java/com/example/android/prueba/MainActivity.java
@@ -17,8 +17,6 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.example.android.prueba.sensors.SensorService;
-
 public class MainActivity extends AppCompatActivity implements Fragment_cardInfo.OnFragmentInteractionListener{
 
     private DrawerLayout drawerLayout;
@@ -26,13 +24,14 @@ public class MainActivity extends AppCompatActivity implements Fragment_cardInfo
     private ListView lista;
     private ArrayAdapter<String> adapter;
 
-    //PABLO
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //crear login
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
 
         agregarToolbar();//encapsulación
 
@@ -57,8 +56,8 @@ public class MainActivity extends AppCompatActivity implements Fragment_cardInfo
         lista.setAdapter(adapter);*/
 
         //PABLO
-        Intent intent = new Intent(this, SensorService.class);
-        startService(intent);
+        //Intent intentSensor = new Intent(this, SensorService.class);
+        //startService(intentSensor);
 
         // PRUEBA POST
         //Intent intent = new Intent(this, ApiService.class);
