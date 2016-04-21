f5467faec2d648d416d90d75fc7719db9b1d7a7e
manuma02 <manuma02@ucm.es>
2016-04-21 15:40:25 +0200

    inserts cambiados. Problema tildes con utf-8

--- rest_api/bbdd_population.sql
+++ rest_api/bbdd_population.sql
@@ -5,16 +5,16 @@ use malarm;
 #Users
 
 insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Juan", "Rodriguez", "jr@malarm.com", "20/10/1990", 0, 65, "12345678J", "secret", "salt", 0);
-insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Manuel", "Martinez", "manuma02@ucm.es", "07/05/1992", 0, 80, "11868634J", "secret", "salt", 1);
+insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Manuel", "Martinez Sanchez", "manuma02@ucm.es", "07/05/1992", 0, 80, "11868634J", "secret", "salt", 1);
 insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Diego", "Diaz", "diediaz@malarm.com", "18/10/1993", 0, 60, "12345678D", "secret", "salt", 1);
 insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Pablo", "Panero", "pepanero@malarm.com", "19/02/1990", 0, 70, "12345678P", "secret", "salt", 2);
-insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Juan Pedro", "Guitierrez", "jprodriguez@malarm.com", "15/10/1998", 0, 60, "12345678Z", "secret", "salt", 2);
+insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Juan Pedro", "Gutierrez", "jprodriguez@malarm.com", "15/10/1998", 0, 60, "12345678Z", "secret", "salt", 2);
 insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Silvia", "Costa", "silcosta@malarm.com", "11/05/2987", 1, 55, "12345678S", "secret", "salt", 3);
 insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Sara", "Flores", "saflores@malarm.com", "07/09/1994", 1, 47, "12345678F", "secret", "salt", 4);
-insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Paz", "Sánchez García", "pazsanga@gmail.com", "15/02/1962", 1, 47, "50305550L", "secret", "salt", 0);
-insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Manuel", "Martínez Zurimendi", "manuelzuri.martinez@ono.es", "02/10/1960", 0, 47, "50301055D", "secret", "salt", 0);
-insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Victoria", "García Contreras", "prueba@malarm.com", "24/01/1933", 1, 47, "02162236Y", "secret", "salt", 0);
-insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Juan Domingo", "Sánchez Lorenzo", "prueba@malarm.com", "12/11/1934", 0, 47, "00625351G", "secret", "salt", 0);
+insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Paz", "Sanchez Garcia", "pazsanga@gmail.com", "15/02/1962", 1, 47, "50305550L", "secret", "salt", 0);
+insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Manuel", "Martinez Zurimendi", "manuelzuri.martinez@ono.es", "02/10/1960", 0, 47, "50301055D", "secret", "salt", 0);
+insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Victoria", "Garcia Contreras", "prueba@malarm.com", "24/01/1933", 1, 47, "02162236Y", "secret", "salt", 0);
+insert into usuario (nombre, apellidos, email, fechaNacimiento, sexo, peso, DNI, secret, salt, estado) values ("Juan Domingo", "Sanchez Lorenzo", "prueba@malarm.com", "12/11/1934", 0, 47, "00625351G", "secret", "salt", 0);
 
 #Disease
 
